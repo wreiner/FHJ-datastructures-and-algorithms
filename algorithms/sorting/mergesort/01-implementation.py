060acf2bd4f389cb8ad8b23cec412cf6cbf3f984
Walter Reiner <walter.reiner@wreiner.at>
2023-04-30 12:59:09 +0200

    Move mergesort

---
+++ algorithms/sorting/mergesort/01-implementation.py
@@ -0,0 +1,26 @@
+# Merge Sort
+
+## Definition der Problemstellung zum Algorithmus
+
+## Praktische Anwendungsbeispiele
+
+-
+
+## Beschreibung der Funktionsweise des Algorithmus
+
+- ein Divide and Conquer Algorithmus
+- das Eingabearray wird rekursiv solange in annähernd gleich große Teile geteilt bis jeder Teilbereich nur mehr ein Element enthält
+- Vergleiche die beiden sortierten Hälften folgendermaßen
+    - das erste Element der linken Hälfte wird mit dem ersten Element der rechten Hälfte verglichen
+        - das kleinere der Elemente wird zuerst in eine Ergebnisliste gespeichert
+            - danach wird der Suchindex auf der Seite um eines erhöht aus dem das Element entnommen wurde
+                - jetzt werden die beiden Index-Values wieder miteinander verglichen
+                    - wenn alle Elemente verglichen wurden wird die Ergebnisliste zurückgegeben
+
+## Ablaufbeispiele mit konkreten Parametern, die auch eventuelle Sonderfälle beinhalten
+
+- []()
+
+## Analyse (Best-, Worst-, Average-Case)
+
+
